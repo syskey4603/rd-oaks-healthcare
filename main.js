ed83f9a0c16fbeb0637f3c019bb874e68d7b47df
syskey <83065600+syskey4603@users.noreply.github.com>
2023-01-21 11:54:32 +0530

    added parameters for api

--- main.js
+++ main.js
@@ -6,7 +6,26 @@ function getanalysis(entertedsymptoms) {
     oHttp.setRequestHeader("Content-Type", "application/json");
     oHttp.setRequestHeader("Authorization", "Bearer " + "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")
 
-    
+    var sModel = "text-davinci-003";
+    var iMaxTokens = 2048;
+    var sUserId = "1";
+    var dTemperature = 0.5;    
 
+    var data = {
+        model: sModel,
+        prompt: "my symptoms are " + entertedsymptoms + " what are the possible diseases and which specialist should be consulted",
+        max_tokens: iMaxTokens,
+        user: sUserId,
+        temperature:  dTemperature,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,  
+        stop: ["#", ";"] 
+    }
+
+    document.getElementById('diagnosis').innerHTML = "thinking...";
+    oHttp.send(JSON.stringify(data));
 
 }
+
+
+
