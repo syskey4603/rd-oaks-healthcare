b6993636952e8274a57413ceb5a324a2fed99060
syskey <83065600+syskey4603@users.noreply.github.com>
2023-01-21 12:55:14 +0530

    completed symptom checker backend

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
-function getanalysis(entertedsymptoms) {
-    var returnedtext = '';
+function analyzesymptoms(query) {
+    var returntext = '';
     var oHttp = new XMLHttpRequest();
     oHttp.open("POST", "https://api.openai.com/v1/completions");
     oHttp.setRequestHeader("Accept", "application/json");
     oHttp.setRequestHeader("Content-Type", "application/json");
     oHttp.setRequestHeader("Authorization", "Bearer " + "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")
+
+    oHttp.onreadystatechange = function () {
+        if (oHttp.readyState === 4) {
+            var oJson = {}
+
+            try {
+                oJson = JSON.parse(oHttp.responseText);
+            } catch (ex) {
+                returntext += "Error: " + ex.message
+            }
+            if (oJson.choices && oJson.choices[0].text) {
+                var s = oJson.choices[0].text;
+
+                if (s == "") s = "No response";
+                returntext += "" + s;
+                document.getElementById('diagnosis').innerHTML = returntext;
+
+                
+            }            
+        }
+    };
 
     var sModel = "text-davinci-003";
     var iMaxTokens = 2048;
@@ -13,7 +34,7 @@ function getanalysis(entertedsymptoms) {
 
     var data = {
         model: sModel,
-        prompt: "my symptoms are " + entertedsymptoms + " what are the possible diseases and which specialist should be consulted",
+        prompt: "my symptoms are " + query + " what are the possible diseases and which specialist should be consulted",
         max_tokens: iMaxTokens,
         user: sUserId,
         temperature:  dTemperature,
@@ -28,4 +49,7 @@ function getanalysis(entertedsymptoms) {
 }
 
 
+function diagnoseproblem() {
+    analyzesymptoms(document.getElementById('symptoms').value);
 
+}
